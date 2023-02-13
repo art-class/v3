ba4b5afb6b75b395754ba600bd430c14d02a997e
ParrotDev <71939973+proudparrot2@users.noreply.github.com>
2023-02-13 16:35:43 +0000

    particles and homepage stuff

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,11 +1,12 @@
 var navbar = `
 <link href="https://unpkg.com/movement.css/movement.css" rel="stylesheet">
+
 <nav>
   <div class="container">
     <img src="/icon.png" alt="Logo" data-m="bounce-right" />
     <ul>
       <li data-m="bounce-right"><a href="/">Home</a></li>
-      <li data-m="bounce-right"><a class="active" href="/gs.html">Games</a></li>
+      <li data-m="bounce-right"><a href="/gs.html">Games</a></li>
       <li data-m="bounce-right"><a href="/apps.html">Apps</a></li>
       <li data-m="bounce-right"><a href="/utils.html">Utilities</a></li>
       <li data-m="bounce-right"><a href="/settings.html">Settings</a></li>
