025c861d9268596055766e77fd223f8e986876c2
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2023-02-24 14:34:09 +0000

    more games

--- static/data/games.js
+++ static/data/games.js
@@ -15,6 +15,17 @@ var games = [
     'url': 'https://mazahacka2017.github.io/papas-burgeria/'
   },
   {
+    'id': 'stumbleguys',
+    'title': "Stumble Guys",
+    'url': 'https://nowgg.me/stumble/stumble.html'
+  },
+  {
+    'id': 'callofduty',
+    'title': 'Call of Duty',
+    'url': 'https://nowgg.me/cod/cod.html',
+    'text': 'May take a while to load'
+  },
+  {
     'id': 'impossiblequiz',
     'title': 'The Impossible Quiz',
     'url': 'https://proudparrot2.github.io/msg-archive/g_mes/impossiblequiz1/impossiblequiz1/index.html'
@@ -68,5 +79,35 @@ var games = [
     'id': 'doodlejump',
     'title': 'Doodle Jump',
     'url': 'https://yell0wsuit.page/assets/games/doodle-jump'
+  },
+  {
+    'id': 'slither',
+    'title': 'slither.io',
+    'url': 'https://slither.io'
+  },
+  {
+    'id': 'basketballstars',
+    'title': 'Basketball Stars',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/basketball-stars/index.html'
+  },
+  {
+    'id': 'amongus',
+    'title': 'Among Us',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/among-us/index.html'
+  },
+  {
+    'id': 'rocketleague',
+    'title': 'Rocket League',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/Rocket-League/index.html'
+  },
+  {
+    'id': 'bitlife',
+    'title': 'Bitlife',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/bitlife/index.html'
+  },
+  {
+    'id': 'btd4',
+    'title': 'Bloons TD 4',
+    'url': 'https://raw.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/btd4/index.html'
   }
 ]
