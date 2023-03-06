2e09e204e65aaf3f5e5a59a9142ed09455cee12b
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2023-03-06 16:24:05 +0000

    Changed UV encoding to base64

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,8 +1,8 @@
 self.__uv$config = {
     prefix: '/service/',
     bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.plain.encode,
-    decodeUrl: Ultraviolet.codec.plain.decode,
+    encodeUrl: Ultraviolet.codec.base64.encode,
+    decodeUrl: Ultraviolet.codec.base64.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
