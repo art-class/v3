99d6a33db3f56ac574ad62fa129980d752284170
LEGALISE PIRACY <74979271+Rahib777-7@users.noreply.github.com>
2023-04-02 22:49:15 -0400

    webretro

--- static/data/games.js
+++ static/data/games.js
@@ -7,6 +7,13 @@ var games = [
     'url': 'https://glitchyeet.github.io/IdleBreakout/',
     'image': 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
   },
+  
+  {
+    'id': 'webretro',
+    'title': 'Webretro',
+    'url': 'https://raw.githack.com/Rahib777-7/3kh0-AssetsAGAIN/main/webretro/index.html',
+    'image': 'https://cdn.jsdelivr.net/gh/Rahib777-7/3kh0-AssetsAGAIN@main/webretro/assets/icons/iconm512upscaledm256.png'
+  },
   {
     'id': 'cmm',
     'title': 'CMM Client',
