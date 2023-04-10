53cb16dd7e139cbf26dbe1ccd82b271732db89b2
joetheshmoe <72777401+joetheshmoe@users.noreply.github.com>
2023-04-10 18:10:10 -0400

    Update cloak.js

--- static/js/cloak.js
+++ static/js/cloak.js
@@ -1,17 +1,37 @@
-if (localStorage.getItem("cloaktype") == "clickoff") {
-  var originalTab = document.title
-  var originalFavicon = document.querySelector("link[rel~='icon']").href
-  
-  document.addEventListener('visibilitychange', (event) => {
-    if (document.title != originalTab) {
-      document.title = originalTab
-      document.querySelector("link[rel~='icon']").href = originalFavicon
+function blank() {
+  const url = location.href;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  let inFrame;
+
+  try {
+    inFrame = window !== top;
+  } catch (e) {
+    inFrame = true;
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = window.open("about:blank", name, `width=${width},height=${height}`);
+
+    if (!popup || popup.closed) {
+      alert("Allow popups and redirects to hide this from showing up in your history.");
     } else {
-      document.title = localStorage.getItem("tabname") || originalTab
-      document.querySelector("link[rel~='icon']").href = localStorage.getItem("favicon") || originalTab
+      const doc = popup.document;
+      const iframe = doc.createElement("iframe");
+      const style = iframe.style;
+      const link = doc.createElement("link");
+
+
+      iframe.src = url;
+      style.position = "fixed";
+      style.top = style.bottom = style.left = style.right = 0;
+      style.border = style.outline = "none";
+      style.width = style.height = "100%";
+
+      doc.head.appendChild(link);
+      doc.body.appendChild(iframe);
+      window.location.replace("https://google.com");
     }
-  });
-} else if (localStorage.getItem("cloaktype") == "normal") {
-  document.title = localStorage.getItem("") || "Art Class"
-  document.querySelector("link[rel=~'icon']")
+  }
 }
