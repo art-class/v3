cca2690bde784ec83ec6dcc8fdf199e62344843c
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2023-04-18 16:15:32 +0000

    proxy (in beta)

--- static/js/script.js
+++ static/js/script.js
@@ -12,7 +12,7 @@ function loadcustomapp() {
     var customapps = getObj("customapps")
     var name = prompt("What should this app be named? (required)")
     var url = prompt("What's this app's url? (required)")
-    var id = prompt("What's this app's ID? (required)")
+    var id = prompt("What's this app's ID? \n\nAn app ID is a unique identifier for the app so we can load it (ex: \"google\", or \"vscode\")\n(required)")
     var icon = prompt("What's this app's icon? (optional)")
     var description = prompt("What's this app's description? (optional)")
 
@@ -51,7 +51,7 @@ function loadcustomgame() {
     var customgames = getObj("customgames")
     var name = prompt("What should this game be named? (required)")
     var url = prompt("What's this game's url? (required)")
-    var id = prompt("What's this game's ID? (required)")
+    var id = prompt("What's this game's ID?")
     var icon = prompt("What's this game's icon? (optional)")
     var description = prompt("What's this game's description? (optional)")
 
@@ -96,3 +96,30 @@ function clearcustomgames() {
     setObj("customgames", [])
     console.log("Removed all custom games!")
 }
+
+// https://www.google.com/search?q=
+function search(input, template) {
+    try {
+      // input is a valid URL:
+      // eg: https://example.com, https://example.com/test?q=param
+      return new URL(input).toString();
+    } catch (err) {
+      // input was not a valid URL
+    }
+  
+    try {
+      // input is a valid URL when http:// is added to the start:
+      // eg: example.com, https://example.com/test?q=param
+      const url = new URL(`http://${input}`);
+      // only if the hostname has a TLD/subdomain
+      if (url.hostname.includes(".")) return url.toString();
+    } catch (err) {
+      // input was not valid URL
+    }
+  
+    // input may have been a valid URL, however the hostname was invalid
+  
+    // Attempts to convert the input to a fully qualified URL have failed
+    // Treat the input as a search query
+    return `https://www.google.com/search?q=${encodeURIComponent(input)}`
+  }
