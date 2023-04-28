5cbc5a5d280a6abce48faa3dee0c5e5f6cf1f520
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2023-04-28 12:53:08 +0000

    generate uuids

--- static/js/script.js
+++ static/js/script.js
@@ -9,35 +9,25 @@ function loadcustomapp() {
     if (!getObj("customapps")) {
         setObj("customapps", [])
     }
-    var customapps = getObj("customapps")
     var name = prompt("What should this app be named? (required)")
     var url = prompt("What's this app's url? (required)")
-    var id = prompt("What's this app's ID? \n\nAn app ID is a unique identifier for the app so we can load it (ex: \"google\", or \"vscode\")\n(required)")
     var icon = prompt("What's this app's icon? (optional)")
     var description = prompt("What's this app's description? (optional)")
+   
 
-    if (!name || !url || !id) return alert("All required fields must be filled in")
+    if (!name || !url) return alert("All required fields must be filled in")
     if (name.length > 15) return alert("App name is too long (max 30 characters)")
 
-    if (id.includes(" ")) return alert("App id cannot contain spaces")
     
-    apps.forEach(app => {
-        if (app.id == id) localStorage.setItem("conflict", true);
+    fetch("https://www.uuidtools.com/api/generate/v4")
+    .then(response => response.json())
+    .then(data => {
+        var customapps = getObj("customapps")
+        customapps.push(JSON.parse(`{ "title": "${name} (Custom app)", "url": "${url}", "id": "${data[0]}", "image": "${icon}", "description": "${description}" }`))
+        setObj("customapps", customapps)
+        window.location.href = self.location
     })
-
-    if (localStorage.getItem("conflict")) {
-        console.log("uhoh something went wrong!!")
-        localStorage.removeItem("conflict")
-        return;
-    } else {
-        console.log("everything is good!!")
-        localStorage.removeItem("conflict")
-    }
-
-    customapps.push(JSON.parse(`{ "title": "${name} (Custom app)", "url": "${url}", "id": "${id}", "image": "${icon}", "description": "${description}" }`))
-    setObj("customapps", customapps)
-    console.log("added custom app!1!!!11!")
-    window.location.href = self.location
+    
     
     
     
@@ -48,37 +38,23 @@ function loadcustomgame() {
     if (!getObj("customgames")) {
         setObj("customgames", [])
     }
-    var customgames = getObj("customgames")
     var name = prompt("What should this game be named? (required)")
     var url = prompt("What's this game's url? (required)")
-    var id = prompt("What's this game's ID?")
     var icon = prompt("What's this game's icon? (optional)")
     var description = prompt("What's this game's description? (optional)")
 
-    if (!name || !url || !id) return alert("All required fields must be filled in")
+    if (!name || !url) return alert("All required fields must be filled in")
     if (name.length > 15) return alert("Game name is too long (max 30 characters)")
 
-    if (id.includes(" ")) return alert("Game id cannot contain spaces")
     
-    games.forEach(game => {
-        if (game.id == id) localStorage.setItem("conflict", true);
+    fetch("https://www.uuidtools.com/api/generate/v4")
+    .then(response => response.json())
+    .then(data => {
+        var customgames = getObj("customapps")
+        customgames.push(JSON.parse(`{ "title": "${name} (Custom game)", "url": "${url}", "id": "${data[0]}", "image": "${icon}", "description": "${description}" }`))
+        setObj("customapps", customgames)
+        window.location.href = self.location
     })
-
-    if (localStorage.getItem("conflict")) {
-        console.log("uhoh something went wrong!!")
-        localStorage.removeItem("conflict")
-        return;
-    } else {
-        console.log("everything is good!!")
-        localStorage.removeItem("conflict")
-    }
-
-    customgames.push(JSON.parse(`{ "title": "${name} (Custom game)", "url": "${url}", "id": "${id}", "image": "${icon}", "description": "${description}" }`))
-    setObj("customgames", customgames)
-    console.log("added custom game'!1!!!11!")
-    window.location.href = self.location
-    
-    
     
     
 }
