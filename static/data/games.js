a4bc1fc43f9ba6490d2a5fcd627e6392fd414d80
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2023-05-17 20:12:41 +0000

    search box change

--- static/data/games.js
+++ static/data/games.js
@@ -174,7 +174,7 @@ var games = [
     'title': 'Super Mario 64',
     'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/sm64/index.html',
     'image': 'https://assets1.ignimgs.com/2019/05/31/mario-64---button-1559263987447.jpg',
-    'description': 'Controls: \nMovement: Arrow Keys \nA: X \nB: C \nR: Q \nZ: Space \nStart: Enter \nC-stick: WASD'
+    'description': 'Controls: <br>Movement: Arrow Keys <br>A: X <br>B: C <br>R: Q <br>Z: Space <br>Start: Enter <br>C-stick: WASD<br><br>You might need to go into fullscreen for the keybinds to register'
   },
   {
     'id': 'snowball',
